2d8a7be3730aecfa32cb5d30db2d99073702e3e5
Abdelzaher11 <oabdelzaher68@gmail.com>
2021-11-19 23:46:15 +0200

    version1.0.1

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:health_version_1_0_1/pages/home.dart';
 import 'package:health_version_1_0_1/pages/status.dart';
 // ignore: import_of_legacy_library_into_null_safe
-import 'package:splashscreen/splashscreen.dart';
 
 void main(){
   runApp(MaterialApp(
@@ -20,19 +20,12 @@ class Splash extends StatefulWidget {
 class _SplashState extends State<Splash> {
   @override
   Widget build(BuildContext context) {
-    return SplashScreen(
-      seconds: 5,
-      imageBackground: const AssetImage('assets/splash.jpg'),
-      loaderColor: Colors.transparent,
-      loadingText: const Text(
-        'Welcome',
-        style: TextStyle(
-          color: Colors.white,
-          fontSize: 19,
-          letterSpacing: 0.8,
-        ),
-      ),
-      navigateAfterSeconds: Status(),
+    return AnimatedSplashScreen(
+      animationDuration: const Duration(seconds: 5),
+      backgroundColor: Colors.black,
+      splash: 'assets/splash.jpg',
+      splashIconSize: 500,
+      nextScreen: Status(),
     );
   }
 }
